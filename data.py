ced557bf0346ce83a9f79440078bff307853dd76
EC2 Default User <ec2-user@ip-172-16-14-95.us-west-2.compute.internal>
2019-04-12 07:49:25 +0000

    update

--- data.py
+++ data.py
@@ -3,6 +3,9 @@ import random
 import pandas as pd
 from copy import deepcopy
 from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
+tqdm.pandas()
 
 random.seed(0)
 
@@ -36,16 +39,21 @@ class SampleGenerator(object):
         assert 'userId' in ratings.columns
         assert 'itemId' in ratings.columns
         assert 'rating' in ratings.columns
-
+        print('start')
         self.ratings = ratings
         # explicit feedback using _normalize and implicit using _binarize
         # self.preprocess_ratings = self._normalize(ratings)
+        print('begin preprocess_ratings')
         self.preprocess_ratings = self._binarize(ratings)
+        print('begin setting pools')
         self.user_pool = set(self.ratings['userId'].unique())
         self.item_pool = set(self.ratings['itemId'].unique())
         # create negative item samples for NCF learning
+        print('creating negative items')
         self.negatives = self._sample_negative(ratings)
+        print('split_loo')
         self.train_ratings, self.test_ratings = self._split_loo(self.preprocess_ratings)
+        print('Done!')
 
     def _normalize(self, ratings):
         """normalize into [0, 1] from [0, max_rating], explicit feedback"""
@@ -57,7 +65,9 @@ class SampleGenerator(object):
     def _binarize(self, ratings):
         """binarize into 0 or 1, imlicit feedback"""
         ratings = deepcopy(ratings)
-        ratings['rating'][ratings['rating'] > 0] = 1.0
+        #ratings['rating'][ratings['rating'] > 0] = 1.0
+        _rate = (ratings['rating'] > 0).values * 1.0
+        ratings['rating'] = _rate
         return ratings
 
     def _split_loo(self, ratings):
@@ -66,16 +76,19 @@ class SampleGenerator(object):
         test = ratings[ratings['rank_latest'] == 1]
         train = ratings[ratings['rank_latest'] > 1]
         #test what if i took this away
-        #assert train['userId'].nunique() == test['userId'].nunique()
+        assert train['userId'].nunique() == test['userId'].nunique()
         return train[['userId', 'itemId', 'rating']], test[['userId', 'itemId', 'rating']]
 
     def _sample_negative(self, ratings):
         """return all negative items & 100 sampled negative items"""
+        print('0')
         interact_status = ratings.groupby('userId')['itemId'].apply(set).reset_index().rename(
             columns={'itemId': 'interacted_items'})
+        print('1')
         # find out which products have not been interacted by each of the users
-        interact_status['negative_items'] = interact_status['interacted_items'].apply(lambda x: self.item_pool - x)
-        interact_status['negative_samples'] = interact_status['negative_items'].apply(lambda x: random.sample(x, 99))
+        interact_status['negative_items'] = interact_status['interacted_items'].progress_apply(lambda x: self.item_pool.difference(x))
+        print('2')
+        interact_status['negative_samples'] = interact_status['negative_items'].progress_apply(lambda x: random.sample(x, 99))
         return interact_status[['userId', 'negative_items', 'negative_samples']]
 
     def instance_a_train_loader(self, num_negatives, batch_size):
@@ -101,6 +114,7 @@ class SampleGenerator(object):
         """create evaluate data"""
         test_ratings = pd.merge(self.test_ratings, self.negatives[['userId', 'negative_samples']], on='userId')
         test_users, test_items, negative_users, negative_items = [], [], [], []
+        print('Begin the loop...')
         for row in test_ratings.itertuples():
             test_users.append(int(row.userId))
             test_items.append(int(row.itemId))
