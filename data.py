149018a728de2b09e42988e618f3069a4a774ac3
EC2 Default User <ec2-user@ip-172-16-14-95.us-west-2.compute.internal>
2019-04-17 06:55:04 +0000

    update on complete automation

--- data.py
+++ data.py
@@ -4,6 +4,8 @@ import pandas as pd
 from copy import deepcopy
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
+import os
+import numpy as np
 
 tqdm.pandas()
 
@@ -28,35 +30,105 @@ class UserItemRatingDataset(Dataset):
         return self.user_tensor.size(0)
 
 
-class SampleGenerator(object):
+class SampleGenerator:
     """Construct dataset for NCF"""
 
-    def __init__(self, ratings):
+    def __init__(self, raw_data_dir):
         """
         args:
             ratings: pd.DataFrame, which contains 4 columns = ['userId', 'itemId', 'rating', 'timestamp']
         """
-        assert 'userId' in ratings.columns
-        assert 'itemId' in ratings.columns
-        assert 'rating' in ratings.columns
-        print('start')
-        self.ratings = ratings
+        print('\nInitialising data cleaning')
+        self.ratings = self._clean_raw_data(pd.read_csv(raw_data_dir))
+        assert 'userId' in self.ratings.columns
+        assert 'itemId' in self.ratings.columns
+        assert 'rating' in self.ratings.columns
+        print('\nInitialising data pre-processing')
         # explicit feedback using _normalize and implicit using _binarize
         # self.preprocess_ratings = self._normalize(ratings)
         print('begin preprocess_ratings')
-        self.preprocess_ratings = self._binarize(ratings)
+        self.preprocess_ratings = self._binarize(self.ratings)
         print('begin setting pools')
         self.user_pool = set(self.ratings['userId'].unique())
         self.item_pool = set(self.ratings['itemId'].unique())
         # create negative item samples for NCF learning
         print('creating negative items')
-        self.negatives = self._sample_negative(ratings)
+        self.negatives = self._sample_negative(self.ratings)
         print('split_loo')
         self.train_ratings, self.test_ratings = self._split_loo(self.preprocess_ratings)
         print('Done!')
+        
+        
+    def _clean_raw_data(self, tdc_record):
+        #tdc_record=pd.read_csv('user-item-oct-dec-2018-v2-modified.csv')
+
+        tdc_record.drop(tdc_record[tdc_record[['USER_ID','ITEM_ID']].duplicated()].index, inplace=True)
+
+        def check_users_items_1(df):
+            # user side
+            user_id = df.groupby('USER_ID').count()['ITEM_ID']
+            bad_users=user_id[user_id==1].index
+            # item side
+            item_id = df.groupby('ITEM_ID').count()['USER_ID']
+            bad_items =item_id[item_id==1].index
+            return bad_users, bad_items
+
+        def clear_users_items_1(df, bad_users, bad_items):
+            # iterrows
+            drop_index=[]
+            for ind, row in df.iterrows():
+                if (row['USER_ID'] in bad_users) | (row['ITEM_ID'] in bad_items):
+                    drop_index.append(ind)
+            df_cleaned = df.drop(drop_index)
+            return df_cleaned
+
+        bad_users, bad_items = check_users_items_1(tdc_record)
+        count = 1
+        while ((len(bad_users)>0) | (len(bad_items)>0)):
+            print('Number of bad users: {} \nNumber of bad items: {}'.format(len(bad_users), len(bad_items)))
+            print('Starting clean process {}\n'.format(count))
+            tdc_record = clear_users_items_1(tdc_record, bad_users, bad_items)
+            tdc_record.drop_duplicates(inplace=True)
+            bad_users, bad_items = check_users_items_1(tdc_record)
+            count += 1
+        print('No more rubbish data.')
+        tdc_record.to_csv(os.path.join('data','cleaned_data.csv'), index=False)
+        tdc_record.columns=['uid', 'mid', 'timestamp']
+
+        # Reindex
+        tdc_record = tdc_record.iloc[1:,:]
+        user_id = tdc_record[['uid']].drop_duplicates().reindex()
+        user_id['userId'] = np.arange(len(user_id))
+        tdc_record = pd.merge(tdc_record, user_id, on=['uid'], how='left')
+        item_id = tdc_record[['mid']].drop_duplicates()
+        item_id['itemId'] = np.arange(len(item_id))
+        tdc_record = pd.merge(tdc_record, item_id, on=['mid'], how='left')
+        tdc_record['rating']=1.0
+        tdc_record = tdc_record[['userId', 'itemId', 'rating', 'timestamp']]
+        
+        tdc_record.drop_duplicates(inplace=True)
+
+        tdc_record['rating']=tdc_record['rating'].astype('int32')
+        tdc_record['timestamp']=tdc_record['timestamp'].astype('float64')
+
+        print('Range of userId is [{}, {}]'.format(tdc_record.userId.min(), tdc_record.userId.max()))
+        print('Range of itemId is [{}, {}]'.format(tdc_record.itemId.min(), tdc_record.itemId.max()))
+        print(tdc_record.dtypes)
+        
+        return tdc_record
+    
+    def usr_item_unique(self):
+        num_itemid=len(self.ratings['itemId'].unique())
+        num_userid=len(self.ratings['userId'].unique()) 
+        return num_itemid, num_userid
 
     def _normalize(self, ratings):
-        """normalize into [0, 1] from [0, max_rating], explicit feedback"""
+        """
+        func: normalize into [0, 1] from [0, max_rating], explicit feedback
+        notes: in the future when the interaction datas are more complicated,
+        you can adjust the interactions into two types. one is view, one is message
+        sent to supplier and one is product purchased
+        """
         ratings = deepcopy(ratings)
         max_rating = ratings.rating.max()
         ratings['rating'] = ratings.rating * 1.0 / max_rating
